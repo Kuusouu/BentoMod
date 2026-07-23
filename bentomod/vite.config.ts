b240930259200596fe551756e63f1afece12c213
Omar <90123670+mewclouds@users.noreply.github.com>
2026-07-23 01:32:26 -0400

    chore(frontend): update TypeScript to version 7.0.2
    LIGHTNING MCQUEEN TYPECHECKS!

--- bentomod/vite.config.ts
+++ bentomod/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
 	appType: "spa",
 	envPrefix: ["VITE_", "TAURI_"],
 	build: {
-		target: ["es2021", "chrome100", "safari13"],
+		target: "es2022",
 		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
 		sourcemap: !!process.env.TAURI_DEBUG,
 		outDir: "dist",
